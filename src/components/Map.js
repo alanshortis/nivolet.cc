86ec69e659c89488be41ed66ec1e6542d44f039f
Alan Shortis <alan.shortis@mac.com>
2018-10-27 18:17:34 +0100

    Render a map

---
+++ src/components/Map.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import mapboxgl from "mapbox-gl";
+import Reset from "../styles/Reset.js";
+import MapContainer from "../styles/MapContainer";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
+
+class Map extends Component {
+  state = {
+    center: [6.536751, 45.20945],
+    zoom: 10,
+  };
+
+  componentDidMount() {
+    const { center, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: process.env.REACT_APP_MAPBOX_STYLE,
+      center,
+      zoom,
+    });
+
+    map.on("moveend", () => {
+      this.setState({
+        center: Object.values(map.getCenter()),
+        zoom: map.getZoom(),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Reset />
+        <MapContainer ref={el => (this.mapContainer = el)} />
+      </>
+    );
+  }
+}
+
+export default Map;
---
+++ src/styles/Reset.js
@@ -0,0 +1,6 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const Reset = createGlobalStyle`${reset}`;
+
+export default Reset;
