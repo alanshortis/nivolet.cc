88a501a78515bb35ad9393b96cf77003c35f1f59
Alan Shortis <alan.shortis@mac.com>
2021-01-29 23:35:42 +0000

    Set next config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import { StrictMode } from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 
 const Nivolet = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default Nivolet;
